d0c6b2ab5fea42eb63e3ae0546130d90aa76f6b5
jyong0000 <jyong3836@gmail.com>
2023-07-31 13:04:49 +0900

    0731

---
+++ src/main/java/com/deliveryservice/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.deliveryservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	String uploadPath = "file:///C:/delivery/";
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")
+				.addResourceLocations(uploadPath);
+	}
+	
+}
